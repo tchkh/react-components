263ce889206cab2e8cf8fbe58e646aa3187108ae
Nut.Thakit <natthakitzn@gmail.com>
2025-03-10 15:18:12 +0700

    feat: complete exercise

--- src/App.jsx
+++ src/App.jsx
@@ -57,6 +57,12 @@ function MainContent() {
   );
 }
 
+function AddCartButton() {
+  return (
+    <button className="add-to-cart-button-secondary">Add to Cart</button>
+  )
+}
+
 function ProductSection() {
   return (
     <section className="product-section">
@@ -67,29 +73,29 @@ function ProductSection() {
             <img
               src="https://placehold.co/300x300"
               alt="The Lorem Ipsum for photos."
-            />
+              />
             <h3>Premium Bluetooth Headphones</h3>
             <p className="product-price">2,400 Baht</p>
             {/*
           Todo 5: ให้ Declare ตัว Component Button เพื่อ Render ปุ่ม “Add to Cart” ที่ซ้ำกันอยู่
           (จุดที่ 1)
-        */}
+          */}
             {/*
           Todo 6: หลังกจากทำ Todo 5 เสร็จ
           ให้เปลี่ยน Style ของปุ่ม “Add to Card” แก้ไข className ใน Component `Button` ที่สร้างขึ้นมา
           จาก “add-to-cart-button” เป็น “add-to-cart-button-secondary”
-        */}
-            <button className="add-to-cart-button">Add to Cart</button>
+          */}
+            <AddCartButton />
           </div>
           <div className="product-card">
             <img
               src="https://placehold.co/300x300"
               alt="The Lorem Ipsum for photos."
-            />
+              />
             <h3>Classic Leather Watch</h3>
             <p className="product-price">4,000 Baht</p>
             {/* Todo 5: (จุดที่ 2) */}
-            <button className="add-to-cart-button">Add to Cart</button>
+            <AddCartButton />
           </div>
           <div className="product-card">
             <img
@@ -99,7 +105,7 @@ function ProductSection() {
             <h3>Organic Green Tea</h3>
             <p className="product-price">79.99 Baht</p>
             {/* Todo 5: (จุดที่ 3) */}
-            <button className="add-to-cart-button">Add to Cart</button>
+            <AddCartButton />
           </div>
         </div>
       </div>
@@ -114,6 +120,7 @@ function Footer() {
     </footer>
   );
 }
+
 
 function App() {
   return (
