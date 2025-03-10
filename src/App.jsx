5b37da8464add58cf1a951de65803045585fd5ae
Suriya <true.many.032@gmail.com>
2025-03-10 15:32:36 +0700

    feat: exercise

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,32 @@
 import "./App.css";
 
-function App() {
+ function Header() {
   return (
-    <div>
-      {/*
-        Todo: 1 ให้ Declare ตัว Component ย่อย 
-       เพื่อแบ่งสัดส่วนของ HTML Element ในส่วนของ <header> 
-      */}
-      <header>
-        <nav>
-          <div className="logo">🚀 XYZ CORP</div>
-          <ul className="nav-list">
-            <li>
-              <a href="#">Home</a>
-            </li>
-            <li>
-              <a href="#">About</a>
-            </li>
-            <li>
-              <a href="#">Services</a>
-            </li>
-            <li>
-              <a href="#">Contact</a>
-            </li>
-          </ul>
-        </nav>
-      </header>
+    <header>
+      <nav>
+        <div className="logo">🚀 XYZ CORP</div>
+        <ul className="nav-list">
+          <li>
+            <a href="#">Home</a>
+          </li>
+          <li>
+            <a href="#">About</a>
+          </li>
+          <li>
+            <a href="#">Services</a>
+          </li>
+          <li>
+            <a href="#">Contact</a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
 
-      {/*
-        Todo 2: ให้ Declare ตัว Component ย่อย
-        เพื่อแบ่งสัดส่วนของ HTML Element ในส่วนของ <main> 
-      */}
-      <main>
+function Main() {
+  return (
+    <main>
         <div className="company-detail-left">
           <h1>Welcome to XYZ Corp</h1>
           <p>Your Trusted Partner in Excellence</p>
@@ -60,64 +55,95 @@ function App() {
           <button className="learn-button">Learn More</button>
         </div>
       </main>
+  )
+}
+function Button() {
+  return (
+    <button className="add-to-cart-button-secondary">Add to Cart</button>
+  );
+}
+function Products() {
+  return (
+    <section className="product-section">
+    <div className="product-section-container">
+      <h2 className="product-featured-heading">Featured Products</h2>
+      <div className="product-card-list">
+        <div className="product-card">
+          <img
+            src="https://placehold.co/300x300"
+            alt="The Lorem Ipsum for photos."
+          />
+          <h3>Premium Bluetooth Headphones</h3>
+          <p className="product-price">2,400 Baht</p>
+          {/*
+            Todo 5: ให้ Declare ตัว Component Button เพื่อ Render ปุ่ม “Add to Cart” ที่ซ้ำกันอยู่
+            (จุดที่ 1)
+          */}
+          {/*
+            Todo 6: หลังกจากทำ Todo 5 เสร็จ
+            ให้เปลี่ยน Style ของปุ่ม “Add to Card” แก้ไข className ใน Component `Button` ที่สร้างขึ้นมา
+            จาก “add-to-cart-button” เป็น “add-to-cart-button-secondary”
+          */}
+          <Button />
+        </div>
+        <div className="product-card">
+          <img
+            src="https://placehold.co/300x300"
+            alt="The Lorem Ipsum for photos."
+          />
+          <h3>Classic Leather Watch</h3>
+          <p className="product-price">4,000 Baht</p>
+          {/* Todo 5: (จุดที่ 2) */}
+          <Button />
+        </div>
+        <div className="product-card">
+          <img
+            src="https://placehold.co/300x300"
+            alt="The Lorem Ipsum for photos."
+          />
+          <h3>Organic Green Tea</h3>
+          <p className="product-price">79.99 Baht</p>
+          {/* Todo 5: (จุดที่ 3) */}
+          <Button />
+        </div>
+      </div>
+    </div>
+  </section>
+  )
+}
+
+function Footer() {
+  return (
+    <footer>
+        <p>&copy; 2023 Your Ecommerce Store. All rights reserved.</p>
+      </footer>
+  );
+}
+function App() {
+  return (
+    <div>
+      {/*
+        Todo: 1 ให้ Declare ตัว Component ย่อย 
+       เพื่อแบ่งสัดส่วนของ HTML Element ในส่วนของ <header> 
+      */}
+      <Header />
+      {/*
+        Todo 2: ให้ Declare ตัว Component ย่อย
+        เพื่อแบ่งสัดส่วนของ HTML Element ในส่วนของ <main> 
+      */}
+      <Main />
 
       {/*
         Todo 3: ให้ Declare ตัว Component ย่อย
         เพื่อแบ่งสัดส่วนของ HTML Element ในส่วนของ <section className="product-section"> 
       */}
-      <section className="product-section">
-        <div className="product-section-container">
-          <h2 className="product-featured-heading">Featured Products</h2>
-          <div className="product-card-list">
-            <div className="product-card">
-              <img
-                src="https://placehold.co/300x300"
-                alt="The Lorem Ipsum for photos."
-              />
-              <h3>Premium Bluetooth Headphones</h3>
-              <p className="product-price">2,400 Baht</p>
-              {/*
-                Todo 5: ให้ Declare ตัว Component Button เพื่อ Render ปุ่ม “Add to Cart” ที่ซ้ำกันอยู่
-                (จุดที่ 1)
-              */}
-              {/*
-                Todo 6: หลังกจากทำ Todo 5 เสร็จ
-                ให้เปลี่ยน Style ของปุ่ม “Add to Card” แก้ไข className ใน Component `Button` ที่สร้างขึ้นมา
-                จาก “add-to-cart-button” เป็น “add-to-cart-button-secondary”
-              */}
-              <button className="add-to-cart-button">Add to Cart</button>
-            </div>
-            <div className="product-card">
-              <img
-                src="https://placehold.co/300x300"
-                alt="The Lorem Ipsum for photos."
-              />
-              <h3>Classic Leather Watch</h3>
-              <p className="product-price">4,000 Baht</p>
-              {/* Todo 5: (จุดที่ 2) */}
-              <button className="add-to-cart-button">Add to Cart</button>
-            </div>
-            <div className="product-card">
-              <img
-                src="https://placehold.co/300x300"
-                alt="The Lorem Ipsum for photos."
-              />
-              <h3>Organic Green Tea</h3>
-              <p className="product-price">79.99 Baht</p>
-              {/* Todo 5: (จุดที่ 3) */}
-              <button className="add-to-cart-button">Add to Cart</button>
-            </div>
-          </div>
-        </div>
-      </section>
+      <Products />
 
       {/*
         Todo 4: ให้ Declare ตัว Component ย่อย
         เพื่อแบ่งสัดส่วนของ HTML Element ในส่วนของ <footer> 
       */}
-      <footer>
-        <p>&copy; 2023 Your Ecommerce Store. All rights reserved.</p>
-      </footer>
+      <Footer />
     </div>
   );
 }
